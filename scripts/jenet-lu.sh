15ac120ee69c0eec670ac6172fe33a4ebbd62b63
yizhanghong <865362414@qq.com>
2025-04-29 09:22:39 +0800

    Create jenet-lu.sh

---
+++ scripts/jenet-lu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=================================================
+# File name: jenet-lu.sh
+# System Required: Linux
+# Version: 1.0
+# Lisence: MIT
+# Author: SuLingGG
+# Blog: https://mlapp.cn
+#=================================================
+echo '修改主机名'
+sed -i "s/hostname='OpenWrt'/hostname='JENET'/g" ./package/base-files/files/bin/config_generate
+echo '修改dts'
+rm -rf target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-opc-h69k.dts
+cp -f $GITHUB_WORKSPACE/configs/jenet-lu.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-opc-h69k.dts
