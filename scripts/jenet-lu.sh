3e606734aa11c8dfea9dc193ca9e87d847e1717e
yizhanghong <865362414@qq.com>
2025-04-29 09:52:50 +0800

    Update jenet-lu.sh

--- scripts/jenet-lu.sh
+++ scripts/jenet-lu.sh
@@ -7,6 +7,8 @@
 # Author: SuLingGG
 # Blog: https://mlapp.cn
 #=================================================
+#更改默认地址为192.168.100.1
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 echo '修改主机名'
 sed -i "s/hostname='OpenWrt'/hostname='JENET'/g" ./package/base-files/files/bin/config_generate
 echo '修改dts'
