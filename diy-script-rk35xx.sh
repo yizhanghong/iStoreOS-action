9fafb443ea170481cd599ba0e81ec5aba0ad3e0e
yizhanghong <865362414@qq.com>
2025-05-15 15:29:55 +0800

    Update diy-script-rk35xx.sh

--- diy-script-rk35xx.sh
+++ diy-script-rk35xx.sh
@@ -16,47 +16,12 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# 添加imb3588的dts
-echo "
-define Device/yx_imb3588
-\$(call Device/rk3588)
-  DEVICE_VENDOR := YX
-  DEVICE_MODEL := IMB3588
-  SUPPORTED_DEVICES += yx,imb3588
-  DEVICE_DTS := rk3588-imb3588
-  DEVICE_PACKAGES := kmod-r8125 kmod-nvme kmod-hwmon-pwmfan kmod-thermal kmod-rkwifi-bcmdhd-pcie rkwifi-firmware-ap6275p
-  IMAGE/sysupgrade.img.gz := boot-combined | boot-script rk3588 | pine64-img | gzip | append-metadata
-endef
-TARGET_DEVICES += yx_imb3588
-" >>  target/linux/rockchip/image/rk35xx.mk
- 
-cp -f $GITHUB_WORKSPACE/dts/rk3588-imb3588.dts target/linux/rockchip/dts/rk3588/rk3588-imb3588.dts
-
 git clone --depth=1 -b main https://github.com/linkease/istore-packages package/istore-packages
 git clone --depth=1 -b dev https://github.com/jjm2473/luci-app-diskman package/diskman
 git clone --depth=1 -b dev4 https://github.com/jjm2473/OpenAppFilter package/oaf
 git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
 git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
 git clone --depth=1 -b main https://github.com/jjm2473/openwrt-apps package/openwrt-apps
-
-#添加qmodem
-git clone --depth=1 -b main https://github.com/FUjr/QModem package/modem
-echo "
-CONFIG_TARGET_DEVICE_rockchip_rk35xx_DEVICE_yx_imb3588=y
-CONFIG_PACKAGE_luci-i18n-qmodem-zh-cn=y
-CONFIG_PACKAGE_luci-app-qmodem=y
-CONFIG_PACKAGE_luci-app-qmodem_INCLUDE_vendor-qmi-wwan=y
-# CONFIG_PACKAGE_luci-app-qmodem_INCLUDE_generic-qmi-wwan is not set
-CONFIG_PACKAGE_luci-app-qmodem_USE_TOM_CUSTOMIZED_QUECTEL_CM=y
-# CONFIG_PACKAGE_luci-app-qmodem_USING_QWRT_QUECTEL_CM_5G is not set
-# CONFIG_PACKAGE_luci-app-qmodem_USING_NORMAL_QUECTEL_CM is not set
-# CONFIG_PACKAGE_luci-app-qmodem_INCLUDE_ADD_PCI_SUPPORT is not set
-# CONFIG_PACKAGE_luci-app-qmodem_INCLUDE_ADD_QFIREHOSE_SUPPORT is not set
-CONFIG_PACKAGE_luci-app-qmodem-hc=y
-CONFIG_PACKAGE_luci-app-qmodem-mwan=y
-CONFIG_PACKAGE_luci-app-qmodem-sms=y
-CONFIG_PACKAGE_luci-app-qmodem-ttl=y
-" >> .config
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
